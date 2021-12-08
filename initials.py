b5475129360ccf0d36dd3b015661306dbee62b40
gmorb <85683495+gmorb@users.noreply.github.com>
2021-12-07 23:02:45 -0800

    Add files via upload
    my first python project

---
+++ initials.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+print('J J J J J     R R R R ')
+
+print('    J         R      R')
+
+print('    J         R      R')
+
+print('    J         R R R R')
+
+print('J   J         R   R')
+
+print('J   J         R    R')
+
+print('    J         R      R')
